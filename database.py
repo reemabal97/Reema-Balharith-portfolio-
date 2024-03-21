2b4b402f4cf08932b125487ad6f75b81ec09ac54
reemabal97 <160021553+reemabal97@users.noreply.github.com>
2024-03-21 11:03:13 +0000

    receive message and save to db

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine,text
 import os
+
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string)
+
 
 def load_projects_from_db():
   with engine.connect() as conn:
@@ -11,4 +13,19 @@ def load_projects_from_db():
     for row in result.all():
       myprojects.append(dict(row._mapping))
     return myprojects
-  
+
+
+def load_project_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from myprojects where id = {id}"))
+    rows = result.all()
+    return dict(rows[0]._mapping)
+
+def add_massage_to_db(data):
+  with engine.connect() as conn:
+    sql=text( f"INSERT INTO massages (Full_Name,email,massage) VALUES (\'{data['Full Name']}\',\'{data['email']}\',\'{data['massage']}\')"
+    )
+
+
+    conn.execute(sql)
+    conn.commit()
